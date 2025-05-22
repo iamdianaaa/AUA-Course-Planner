d285a32267bd875ba137b67da2ade9714f06229c
Arman Sujoyan <asujoyan@gmail.com>
2025-05-22 10:05:56 +0400

    chore: Add environment config

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from src import create_app
+import os
 
 app = create_app()
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=os.getenv("PORT", 5000), debug=True)
